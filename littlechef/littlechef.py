462b1cf804e96ec407f12cacb912069eed06dd1c
michael <connellmp@gmail.com>
2020-09-01 15:07:22 -0700

    initial dataframe generator design complete

--- littlechef/littlechef.py
+++ littlechef/littlechef.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import json
 import csv
+import datetime
+import random
+import calendar
 
 
 class make(object):
-
     def a_list(self, length=101):
         self.length = length
         return [i for i in range(self.length)]
@@ -41,8 +43,31 @@ class make(object):
         ]
         return {a: n for a, n in enumerate(alpha)}
 
-    def a_df(self):
-        pass
+    def a_df(self, n=100):
+
+        self.n = n
+
+        def date_generator():
+            month = random.randint(1, 12)
+            year = random.randint(1950, datetime.datetime.now().year)
+            dates = calendar.Calendar().itermonthdates(year, month)
+            return random.choice([date for date in dates if date.year == year])
+
+        return pd.DataFrame(
+            [
+                [
+                    1,
+                    15.5,
+                    "xyz",
+                    False,
+                    True,
+                    np.nan,
+                    date_generator(),
+                ]
+                for _ in range(n)
+            ],
+            columns=["A", "B", "C", "D", "E", "F", "G"],
+        )
 
     def a_matrix(self):
         pass
